5d9684223f95c63d9abe26bf26f5b8051be119df
Windsvept <junsubjjang@gmail.com>
2018-10-01 19:59:23 -0700

    tested both cql psql

--- database/cqlAddData.js
+++ database/cqlAddData.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 const split = require('split')
-const cassandra = require('cassandra-driver');4
-const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'sdc2' });
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'sdc' });
 let queries = [];
 
-let addData = fs.createReadStream('/media/winds/Disk2/SDC/reviewData.csv', {flags: 'r', encoding: 'utf-8'});
+let addData = fs.createReadStream('/media/winds/Disk2/SDC/reviewData.json', {flags: 'r', encoding: 'utf-8'});
 var lineStream = addData.pipe(split());
 let count = 0;
+let addCount = 500000;
+
 lineStream.on('data', function(chunk) {
-  // var rev = JSON.parse(chunk);
-  // var id = rev.reviewId;
-  let tempChunk = chunk.substring(1, chunk.length-1);
+  let tempChunk = chunk.substring(0, chunk.length-1);
   queries.push({ 
     query: "INSERT INTO reviews (productId, productName, reviewId, username, stars, title, text, timestamp, numHelpful, verifiedPurchase, imageUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
     params: tempChunk.split(',')
         // [rev.productId, rev.productName, rev.reviewId, rev.username, rev.stars, rev.title, rev.text, rev.timestamp, rev.numHelpful, rev.verifiedPurchase, rev.imageUrl]
-
-  })
-  // if(id % 500000 === 0) {
-  //   console.log('at ' + id / 500000 + '%' + '\n');
-  // }
+  });
   count++;
-  if(count === 100) {
+  if(count === 100 && addCount > 0) {
+    if(addCount % 5000 === 0) {
+      console.log(100 - (addCount / 5000) + '%')
+    }
+    addCount--;
     lineStream.pause();
     count = 0;
     client.batch(queries, {prepare: true}, function (err, res) {
@@ -30,16 +30,6 @@ lineStream.on('data', function(chunk) {
       lineStream.resume();
     });
   }
-
-  // client.execute(
-  //     "INSERT INTO reviews (productId, productName, reviewId, username, stars, title, text, timestamp, numHelpful, verifiedPurchase, imageUrl) VALUES (?, 'temp', ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-  //     Object.values(rev), { prepare: true }, function (err, res) {
-  //       if(err) {console.log(err); } 
-  //       else { 
-  //         if(id % 100000 === 0) {
-  //           console.log('at ' + id / 100000 + '%' + '\n');
-  //         }
-  //       }
-  //       //Inserted in the cluster
-  //     });
 });
+
+// client.execute("COPY reviews (productId, productName, reviewId, username, stars, title, text, timestamp, numHelpful, verifiedPurchase, imageUrl) FROM '/media/winds/Disk2/SDC/reviewData.csv' WITH DELIMITER = ', ")
--- database/seed.js
+++ database/seed.js
@@ -1,31 +1,21 @@
 const faker = require('faker');
 const fs = require("fs");
-// const pg = require('knex')({
-  //     client: 'pg',
-  //     connection: {
-    //         host : '127.0.0.1',
-    //         user : 'your_database_user',
-    //         password : 'your_database_password',
-    //         database : 'myapp_test'
-    //     },
-    //     searchPath: ['knex', 'public'],
-    // });
-//1 mill 
-let writeTimes = 200;
+ 
+let writeTimes = 200
 let dataNumber = 250000;
+let drainevent = true;
+let dataFile;
 
-
-//var dataFile = fs.createWriteStream('/media/winds/Disk2/SDC/reviewData.csv');
-    
+dataFile = fs.createWriteStream('/media/winds/Disk2/SDC/reviewData.csv');
 let writeData = (i, revId) => {
+  let reviewId = revId
   let csvString = '';
-
   for (;i <= dataNumber; i++) {
-    let productId = Math.floor(revId / 5);
+    let productId = Math.floor(reviewId / 5);
     let review = {
       productId: productId,
-      productName: "Clean-O-Bot " + productId,
-      reviewId: revId,
+      productName: "Clean-O-Bot " + productId + 1,
+      reviewId: reviewId,
       username: faker.internet.userName(),
       stars: Math.floor(Math.random() * 6),
       title: faker.lorem.sentence(),
@@ -35,41 +25,25 @@ let writeData = (i, revId) => {
       verifiedPurchase: Math.random() < 0.5,
       imageUrl: faker.image.avatar(),
     };
-    revId = revId + 1;
-    csvString += `${review.productId}, ${review.productName}, ${review.reviewId}, ${review.username}, ${review.stars}, ${review.title}, ${review.text}, ${review.timestamp}, ${review.numHelpful}, ${review.verifiedPurchase}, ${review.imageUrl}\n`;
-
-    if( i === dataNumber & writeTimes > 0) {
-      writeTimes--;
-      fs.appendFile('/media/winds/Disk2/SDC/reviewData.csv', csvString, (err) => {
-        drainevent = null;
-        csvString = null;
-        review = null;
-        productId = null;
-        console.log(Math.floor((200 - writeTimes)/2));
-        writeData(1, revId);   
-      });
-    
-      // if (!drainevent) {
-        // dataFile.removeAllListeners('drain');
-        // dataFile.once('drain', function() {
-        //   dataFile.removeAllListeners('drain');
-        //   drainevent = null;
-        //   productId = null;
-        // });
-      // } else {
-      //   writeData(1, revId);
-      // }
+    reviewId = reviewId + 1;
+    csvString += `${review.productId}|${review.productName}|${review.reviewId}|${review.username}|${review.stars}|${review.title}|${review.text}|${review.timestamp}|${review.numHelpful}|${review.verifiedPurchase}|${review.imageUrl}\n`;
+    productId = null;
+    review = null;
+  }
+  if(writeTimes > 0) {
+    writeTimes--;
+    drainevent = dataFile.write(csvString);
+    if(!drainevent){
+      drainevent = true;
+      csvString = null;
+      reviewId = null;
+      console.log(Math.floor(revId/ 500000) + '% \n \n')
+      dataFile.once('drain', (err) => {
+        writeData(1, dataNumber * (200 - writeTimes))
+      })
+      return;
     }
-    
-    // Wait for it to drain then start writing data from where we left off
-    // if(i % 500000 === 0) {
-      //   console.log(i / 500000 + ' %', 'time: ' + new Date())
-      // }
   }
 };
   
-// let writeNext = () => {
-
-// }
-
 writeData(1, 0);
