30dc8b6d1e123737802858130c04ad9d00ae0ce0
Windsvept <junsubjjang@gmail.com>
2018-10-09 17:50:28 -0700

    deploying2

--- database/index.js
+++ database/index.js
@@ -8,57 +8,57 @@ const client = new pg.Client({
   port: 5432,
 });
 client.connect();
-// client.query(
-//   `CREATE TABLE products(
-//     id INTEGER PRIMARY KEY, 
-//     productName VARCHAR(40) not null
-//   )`
-// )
-// client.query(
-//   `CREATE TABLE reviews(
-//     idd SERIAL,
-//     productId INTEGER REFERENCES products(id) ON DELETE RESTRICT, 
-//     reviewId INTEGER, 
-//     username TEXT, 
-//     stars SMALLINT, 
-//     title TEXT, 
-//     text TEXT, 
-//     timestamp TEXT, 
-//     numHelpful SMALLINT, 
-//     verifiedPurchase BOOLEAN, 
-//     imageUrl TEXT,
-//     PRIMARY KEY (idd, reviewId)
-//   )`
-// );
+client.query(
+  `CREATE TABLE products(
+    id INTEGER PRIMARY KEY, 
+    productName VARCHAR(40) not null
+  )`
+)
+client.query(
+  `CREATE TABLE reviews(
+    idd SERIAL,
+    productId INTEGER REFERENCES products(id) ON DELETE RESTRICT, 
+    reviewId INTEGER, 
+    username TEXT, 
+    stars SMALLINT, 
+    title TEXT, 
+    text TEXT, 
+    timestamp TEXT, 
+    numHelpful SMALLINT, 
+    verifiedPurchase BOOLEAN, 
+    imageUrl TEXT,
+    PRIMARY KEY (idd, reviewId)
+  )`
+);
 
 // productId, reviewId, username, stars, title, text, timestamp, numHelpful, verifiedPurchase, imageUrl
 
-let getReviews = (productId, cb) => {
-  client.query(`SELECT * FROM products INNER JOIN reviews ON reviews.productId = products.id WHERE id=${productId}`, (err, res) => {
-    if (err) {
-      cb(err, null);
-    }
-    cb(null, res.rows);
-  });
-}
+// let getReviews = (productId, cb) => {
+//   client.query(`SELECT * FROM products INNER JOIN reviews ON reviews.productId = products.id WHERE id=${productId}`, (err, res) => {
+//     if (err) {
+//       cb(err, null);
+//     }
+//     cb(null, res.rows);
+//   });
+// }
 
-let incrementHelpfulness = (reviewId, cb) => {
-  client.query(`SELECT numhelpful FROM reviews WHERE reviewid = ${reviewId}`, (err, res) => {
-    client.query(`UPDATE reviews SET numhelpful = ${res.rows[0].numhelpful+1} WHERE reviewid = ${reviewId}`, (err, res) => {
-      console.log(err)
-      cb(err, res.rows);
-    });
-  });
-}
+// let incrementHelpfulness = (reviewId, cb) => {
+//   client.query(`SELECT numhelpful FROM reviews WHERE reviewid = ${reviewId}`, (err, res) => {
+//     client.query(`UPDATE reviews SET numhelpful = ${res.rows[0].numhelpful+1} WHERE reviewid = ${reviewId}`, (err, res) => {
+//       console.log(err)
+//       cb(err, res.rows);
+//     });
+//   });
+// }
 
-let createReview = (data, cb) => {
-  console.log(data, typeof data.username);
-  client.query(`INSERT INTO reviews (productId, reviewId, username, stars, title, text, timestamp, numHelpful, verifiedPurchase, imageUrl) VALUES (${data.productId}, ${data.reviewId}, '${data.username}', ${data.stars}, '${data.title}', '${data.text}', '${data.timestamp}', ${data.numHelpful}, ${data.verifiedPurchase}, '${data.imageUrl}');`, (err, res) => {
-    console.log(err);
-    cb(err, null);
-  });
-}
+// let createReview = (data, cb) => {
+//   console.log(data, typeof data.username);
+//   client.query(`INSERT INTO reviews (productId, reviewId, username, stars, title, text, timestamp, numHelpful, verifiedPurchase, imageUrl) VALUES (${data.productId}, ${data.reviewId}, '${data.username}', ${data.stars}, '${data.title}', '${data.text}', '${data.timestamp}', ${data.numHelpful}, ${data.verifiedPurchase}, '${data.imageUrl}');`, (err, res) => {
+//     console.log(err);
+//     cb(err, null);
+//   });
+// }
 
-exports.getReviews = getReviews;
-exports.incrementHelpfulness = incrementHelpfulness;
-exports.createReview = createReview;
+// exports.getReviews = getReviews;
+// exports.incrementHelpfulness = incrementHelpfulness;
+// exports.createReview = createReview;
